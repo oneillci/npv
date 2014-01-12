bccc5c0c96af448c5f0af856cb679ed13eb3f00a
Ciaran O'Neill <oneillci@gmail.com>
2014-01-12 22:46:01 +1100

    added math.pow into calculation

--- CiaranONeill.NPV/CiaranONeill.NPV.Calculator/NpvCalculator.cs
+++ CiaranONeill.NPV/CiaranONeill.NPV.Calculator/NpvCalculator.cs
@@ -5,9 +5,15 @@ namespace CiaranONeill.NPV.Calculator
 {
     public class NpvCalculator
     {
-        public double CalculatePresentValue(double cashflow, double rate)
+        
+        public double CalculatePresentValue(double cashflow, double rate, int period = 1, int rollsPerPeriod = 1)
         {
-            var pv = cashflow / (1 + rate);
+            if (rate <= 0)
+                throw new ArgumentException("Rate cannot be <= 0");
+            if (rate > 100)
+                throw new ArgumentException("Rate cannot be > 100");
+
+            var pv = cashflow / Math.Pow(1 + rate, period);
 
             return pv;
         }
--- CiaranONeill.NPV/CiaranONeill.NPV.Tests/NpvCalculatorTests.cs
+++ CiaranONeill.NPV/CiaranONeill.NPV.Tests/NpvCalculatorTests.cs
@@ -8,13 +8,30 @@ namespace CiaranONeill.NPV.Tests
     public class NpvCalculatorTests
     {
         [Fact]
-        public void CalculatePresentValue_ForFirstYear_ReturnsPresentValue()
+        public void CalculatePresentValue_ForFirstYear_ReturnsValue()
         {
             var sut = new NpvCalculator();
-
             var pv = sut.CalculatePresentValue(25, 0.10d);
 
-            Assert.Equal(22.7272, pv, 4);            
+            Assert.Equal(22.72727, pv, 4);            
+        }
+
+        [Fact]
+        public void CalculatePresentValue_ForFirstSecondYear_ReturnsValue()
+        {
+            var sut = new NpvCalculator();
+            var pv = sut.CalculatePresentValue(25, 0.10d, 2);
+
+            Assert.Equal(20.66115, pv, 4);
+        }
+
+        [Fact]
+        public void CalculatePresentValue_ForFirstThirdYear_ReturnsValue()
+        {
+            var sut = new NpvCalculator();
+            var pv = sut.CalculatePresentValue(30, 0.10d, 3);
+
+            Assert.Equal(22.53944, pv, 4);
         }
     }
 }
