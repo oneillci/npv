f5cf320745f9001bd708aea2eaf668ef692f2ede
Ciaran O'Neill <oneillci@gmail.com>
2014-01-12 22:20:28 +1100

    Adding initial unit test

---
+++ CiaranONeill.NPV/CiaranONeill.NPV.Calculator/NpvCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace CiaranONeill.NPV.Calculator
+{
+    public class NpvCalculator
+    {
+        public double CalculatePresentValue(double cashflow, double rate)
+        {
+            var pv = cashflow / (1 + rate);
+
+            return pv;
+        }
+    }
+}
---
+++ CiaranONeill.NPV/CiaranONeill.NPV.Tests/NpvCalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using CiaranONeill.NPV.Calculator;
+using Xunit;
+
+namespace CiaranONeill.NPV.Tests
+{
+    public class NpvCalculatorTests
+    {
+        [Fact]
+        public void CalculatePresentValue_ForFirstYear_ReturnsPresentValue()
+        {
+            var sut = new NpvCalculator();
+
+            var pv = sut.CalculatePresentValue(25, 0.10d);
+
+            Assert.Equal(22.7272, pv, 4);            
+        }
+    }
+}
