b00a6f32a5ffa7272118c206ff0320b23000d801
unknown <coneil@232YF2S.cityofsydney.nsw.gov.au>
2014-01-13 16:32:48 +1100

    adding dates class - still not sure what the purpose is

--- CiaranONeill.NPV/CiaranONeill.NPV.Calculator/NpvCalculator.cs
+++ CiaranONeill.NPV/CiaranONeill.NPV.Calculator/NpvCalculator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CiaranONeill.NPV.Calculator
@@ -15,5 +16,44 @@ namespace CiaranONeill.NPV.Calculator
 
             return pv;
         }
+    }
+
+    public class DateRollCalculator
+    {
+        public IEnumerable<DateTime> GetDates(TimePeriod period)
+        {
+            var startDate = new DateTime(DateTime.Now.Year, 1, 1);
+            var endDate = new DateTime(DateTime.Now.Year + 10, 1, 1);
+            switch(period)
+            {
+                case TimePeriod.Annually:
+                    //return Enumerable.Range(DateTime.Now.Year, 10).Select(x => new DateTime(x, 1, 1));
+                    return GetDates(endDate, 12);
+                case TimePeriod.Quarterly:
+                    return GetDates(endDate, 3);
+                case TimePeriod.Monthly:
+                    return GetDates(endDate, 1);
+                default:
+                    return GetDates(endDate, 12);
+            }
+        }
+
+        private IEnumerable<DateTime> GetDates(DateTime endDate, int increment)
+        {
+            var period = new DateTime(DateTime.Now.Year, 1, 1);
+            var list = new List<DateTime>();
+            while (period < endDate)
+            {
+                yield return period;
+                period = period.AddMonths(increment);
+            }
+        }
+    }
+
+    public enum TimePeriod
+    {
+        Annually,
+        Quarterly,
+        Monthly
     }
 }
