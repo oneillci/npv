078b24fc2247fb6cc3d6985d4233c76cc791fc78
unknown <coneil@232YF2S.cityofsydney.nsw.gov.au>
2014-01-14 19:44:09 +1100

    initial wcf service

---
+++ CiaranONeill.NPV/CiaranONeill.NPV.Silverlight.Web/NpvService.svc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+
+namespace CiaranONeill.NPV.Silverlight.Web
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class NpvService
+    {
+        [OperationContract]
+        public string DoWork()
+        {
+            return "Hello from WCF";
+        }
+
+        [OperationContract]
+        public Customer[] GetCustomers(Customer customer)
+        {
+            return new[]
+            {
+                new Customer { Id = 1, Name = "Ciaran" },
+                new Customer { Id = 2, Name = "Stevie G" }
+            };
+        }
+    }
+}
--- CiaranONeill.NPV/CiaranONeill.NPV.Silverlight/MainPage.xaml.cs
+++ CiaranONeill.NPV/CiaranONeill.NPV.Silverlight/MainPage.xaml.cs
@@ -1,32 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using CiaranONeill.NPV.Calculator;
+using CiaranONeill.NPV.Silverlight.NpvServiceReference;
 
 namespace CiaranONeill.NPV.Silverlight
 {
     public partial class MainPage : UserControl
     {
+        public List<Data> Cashflows { get; set; }
+
         public MainPage()
         {
             InitializeComponent();
 
-            Cashflows = new Dictionary<string, double>();
+            Cashflows = new List<Data>();
 
             var dates = new DateRollCalculator().GetDates(RolloverType.Month).ToList();
             var data = new NpvCalculator().GetRandomData().ToList();
 
             for (int i = 0; i < dates.Count(); i++)
             {
-                Cashflows.Add(dates[i].ToShortDateString(), data[i]);
+                Cashflows.Add(new Data { Period = dates[i], Cashflow = data[i] });
             }
 
-            //Cashflows.Add("Period 1", 200);
-            //Cashflows.Add("Period 2", 250);
             lstCashflow.ItemsSource = Cashflows;
         }
 
-        public Dictionary<string, double> Cashflows { get; set; }
+
+        private async void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            MessageBox.Show(await NpvServiceProxy.GetHello());
+            var customers = await NpvServiceProxy.GetCustomers(new Customer());
+            MessageBox.Show(customers.First().Name);
+        }
+    }
+
+    public class NpvServiceProxy
+    {
+        public static Task<string> GetHello()
+        {
+            var tcs = new TaskCompletionSource<string>();
+
+            var client = new NpvServiceClient();
+
+            client.DoWorkCompleted += (s, e) =>
+            {
+                if (e.Error != null)
+                    tcs.TrySetException(e.Error);
+                else if (e.Cancelled)
+                    tcs.TrySetCanceled();
+                else
+                    tcs.TrySetResult(e.Result);
+            };
+
+            client.DoWorkAsync();
+
+            return tcs.Task;
+        }
+
+        public static Task<ObservableCollection<Customer>> GetCustomers(Customer customer)
+        {
+            var tcs = new TaskCompletionSource<ObservableCollection<Customer>>();
+
+            var client = new NpvServiceClient();
+
+            client.GetCustomersCompleted += (s, e) =>
+            {
+                if (e.Error != null)
+                    tcs.TrySetException(e.Error);
+                else if (e.Cancelled)
+                    tcs.TrySetCanceled();
+                else
+                    tcs.TrySetResult(e.Result);
+            };
+
+            client.GetCustomersAsync(customer);
+
+            return tcs.Task;
+        }
+    }
+
+    public class Data
+    {
+        public DateTime Period { get; set; }
+        public double Cashflow { get; set; }
     }
 }
